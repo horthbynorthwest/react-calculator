4362d0f644c5fde25c553e572d10ac90c6eb613b
Georgie Horth <georgiehorth@gmail.com>
2021-03-02 08:55:09 +0000

    Added failing test for keypad component

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Calculator from './Calculator';
 import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad'
 import '../setupTests'
 
 describe('Calculator', () => {
@@ -13,8 +14,8 @@ describe('Calculator', () => {
         expect(wrapper.find('div').length).toEqual(1);
     });
 
-    it('should render the Display Component', () => {
-        expect(wrapper.containsMatchingElement(<Display displayValue={wrapper.instance().state.displayValue} />
+    it('should render the Display and Keypad Components', () => {
+        expect(wrapper.containsAllMatchingElements([<Display displayValue={wrapper.instance().state.displayValue} />, <Keypad />]
         )).toEqual(true)
     });
 });
