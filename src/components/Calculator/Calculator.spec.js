1ac06177531a9c4b4d8d91675b11701303c61df3
Georgie Horth <georgiehorth@gmail.com>
2021-03-04 15:11:17 +0000

    Added mounted tests for onclick methods for when number keys are pressed

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Calculator from './Calculator';
 import Display from '../Display/Display';
 import Keypad from '../Keypad/Keypad'
@@ -26,4 +26,49 @@ describe('Calculator', () => {
         />]
         )).toEqual(true)
     });
+});
+
+describe('Mounted Calculator', () => {
+    let wrapper;
+
+    beforeEach(() => wrapper = mount(<Calculator />));
+
+    it('calls updateDisplay when a number key is clicked', () => {
+        // created a spy/double of the updateDisplay function
+        const spy = jest.spyOn(wrapper.instance(), 'updateDisplay');
+        // use forceUpdate to re-render the instance within this test
+        wrapper.instance().forceUpdate();
+        // won't have been called as it's an on click method
+        expect(spy).toHaveBeenCalledTimes(0);
+        // use simulate to simulate a person clicking a key
+        wrapper.find('.number-key').first().simulate('click');
+        // now has been called as it's an on click method
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    // it('calls setOperator when an operator key is clicked', () => {
+    //     // created a spy/double of the updateDisplay function
+    //     const spy = jest.spyOn(wrapper.instance(), 'setOperator');
+    //     // use forceUpdate to re-render the instance within this test
+    //     wrapper.instance().forceUpdate();
+    //     // won't have been called as it's an on click method
+    //     expect(spy).toHaveBeenCalledTimes(0);
+    //     // use simulate to simulate a person clicking a key
+    //     wrapper.find('.operator-key').first().simulate('click');
+    //     // now has been called as it's an on click method
+    //     expect(spy).toHaveBeenCalledTimes(1);
+    // });
+
+    // it('calls callOperator when the submit key is clicked', () => {
+    //     // created a spy/double of the updateDisplay function
+    //     const spy = jest.spyOn(wrapper.instance(), 'callOperator');
+    //     // use forceUpdate to re-render the instance within this test
+    //     wrapper.instance().forceUpdate();
+    //     // won't have been called as it's an on click method
+    //     expect(spy).toHaveBeenCalledTimes(0);
+    //     // use simulate to simulate a person clicking a key
+    //     wrapper.find('.submit-key').first().simulate('click');
+    //     // now has been called as it's an on click method
+    //     expect(spy).toHaveBeenCalledTimes(1);
+    // });
 });
